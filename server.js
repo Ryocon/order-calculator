169b9f225526e1bf136cd53c1e1b9651f1238d7c
roconn <roconn25@gmail.com>
2024-09-11 20:54:26 +0100

    loops in place working out average spend

--- server.js
+++ server.js
@@ -22,13 +22,42 @@ function getData() {
     fetch("https://fauxdata.codelayer.io/api/orders", requestOptions)
       .then(response => response.json()) 
       .then(result => {
+
+        const orders = result.orders
+        let total = 0
+        let customers = 0
+
+        orders.forEach(order => {
+            let orderTotal = 0
+
+            order.items.forEach(item => {
+                orderTotal += parseFloat(item.price)
+            })
+
+            console.log(`Customer: ${order.customer.name}, Total spent: ${orderTotal}`)
+
+            total += orderTotal
+            customers++
+
+            
+        })
+
+        const average = total / customers
+            console.log(`Average amount spent: ${average.toFixed(2)}`)
+
         // console.log(result.orders.items)
         // console.log(result)
-        for (let i = 0; i < result.orders.length; i++) {
-            console.log(result.orders[i].items)
-        }
-      } )  
-      .catch(error => console.log('error', error));
+        // for (let i = 0; i < result.orders.length; i++) {
+        //     console.log(result.orders[i].items)
+        // }
+
+
+      } 
+    )  
+
+      .catch(error => console.log('error', error)
+    
+    );
   }
 
   getData()
